daf6cd5af6ebef1d4f74afc27d84a91a8ec3301c
fernando <64593873+carabedo@users.noreply.github.com>
2023-04-18 01:07:49 -0300

    v0 vtex

--- vtex/Home/main.js
+++ vtex/Home/main.js
@@ -1,36 +1,53 @@
-function search(){
-    console.log('hola')
-}
+// // listener del form 
+// window.addEventListener("load", function () {
+//     let busqueda = document.querySelector("form");
+//     // miro el evento submit
+//     busqueda.addEventListener("submit", function (e) {
+//     e.preventDefault()
 
-let json 
+//     // evito que mande el formulario al backend
 
-const getProductos = async (producto) => {
+//     (async () => {
+//     let producto = document.getElementById("input").value
+//     await fetchProductos(producto);
+//     console.log(productosData);
+//     })();
+//     // fetch siempre tiene 2 then, en el segundo va la accion con la data
+
+
+
+//     });
+//   });
+
+let productosData
+
+
+const fetchProductos = async (producto) => {
+    
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json")
+    // myHeaders.append('Access-Control-Allow-Origin','*')
     var requestOptions = {
         method: 'GET',
+        headers: myHeaders,
         redirect: 'follow'
       };
-      
     const response = await fetch('https://7pyngmccwa.execute-api.us-east-1.amazonaws.com/default/apitest?q='+producto, requestOptions);
     const json = await response.json();
-  
-    return json;
-  };
-// listener del form 
-window.addEventListener("load", function () {
-    let busqueda = document.querySelector("form");
-    // miro el evento submit
-    busqueda.addEventListener("submit", function (e) {
-      // evito que mande el formulario al backend
-      e.preventDefault();
-      let producto = document.getElementById("input")
- 
-      json=getProductos(producto)
+    productosData = json;
+};
 
-      // fetch siempre tiene 2 then, en el segundo va la accion con la data
+const btnBusca = document.getElementById('boton');
+btnBusca.addEventListener('click', async (e) => {
+  e.preventDefault()
+  let producto = document.getElementById("input").value
 
-
-
-    });
-  });
-  
-  
+  await fetchProductos(producto);
+  console.log(productosData);
+  for(var key in productosData) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(productosData[key].name+' dia: '+productosData[key].precio_dia+'$ carrefour: '+productosData[key].precio_carrefour+'$ chango: '+productosData[key].precio_chango);
+    node.appendChild(textnode);
+    document.getElementById("lista").appendChild(node);
+   }
+});
