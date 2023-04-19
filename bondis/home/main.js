2378dc98afcfb8da8c642fc5f36def388068c608
carabedo <carabedo@gmail.com>
2023-04-19 17:11:20 -0300

    mobile-location

--- bondis/home/main.js
+++ bondis/home/main.js
@@ -4,3 +4,15 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+map.locate({setView: true, maxZoom: 16});
+
+
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.circle(e.latlng, 10).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
