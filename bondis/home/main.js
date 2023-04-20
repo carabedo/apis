6c9dc7bc0053958b3217f48577813eb88a934c5b
fernando <64593873+carabedo@users.noreply.github.com>
2023-04-19 23:04:18 -0300

    v3

--- bondis/home/main.js
+++ bondis/home/main.js
@@ -6,13 +6,44 @@ L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-map.locate({setView: true, maxZoom: 16});
+map.locate({setView: true, maxZoom: 15});
 
 
 function onLocationFound(e) {
     var radius = e.accuracy;
 
-    L.circle(e.latlng, 10).addTo(map);
+    L.circle(e.latlng,  {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 1,
+        radius: 10
+    }).addTo(map);
+    lat=e.latlng['lat']
+    lon=e.latlng['lon']
+    radio=0.01
+    getBondis(lat,lon,radio,map)
+
 }
 
 map.on('locationfound', onLocationFound);
+
+
+function getBondis(lat,lon,radio,map){
+
+    lat=-34.654756208531126
+    lon= -58.34646820091482
+    radio=0.01
+
+    url_base='http://cuandosubo.sube.gob.ar/onebusaway-api-webapp/api/where/'
+    url_endpoint='trips-for-location.json?'
+    url=url_base+url_endpoint+'lat='+lat.toString()+'&lon='+lon.toString()+'&latSpan='+ radio.toString() + '&lonSpan='+radio.toString()+'&app_ver=137&includeStatus=true&app_uid=786390b9-e7a3-44b6-bc1e-4ce5eed18b52&version=2&key=CuandoSUBO-Android-9b8a8543-b111-4caf-80b0-b81978707a0c'
+
+
+    L.circle([lat,lon], {
+        color: 'black',
+        fillColor: 'black',
+        fillOpacity: 0.5,
+        radius: 5
+    }).addTo(map).bindTooltip("culito2 ");
+
+}
