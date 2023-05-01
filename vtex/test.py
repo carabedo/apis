2d7648468a53ffb554f477b85a1ab7e9f129c6dc
carabedo <carabedo@gmail.com>
2023-04-30 23:02:41 -0300

    sdfgs

--- vtex/test.py
+++ vtex/test.py
@@ -1,28 +1,83 @@
-import sys
-from main import *
+import requests as rq
+import pandas as pd
+import time
+from datetime import datetime
 
-producto=sys.argv[1]
-print('buscando', producto )
 
-try:
-    df,lista,dat=get_dia(producto)
-    print('dia%', len(lista),' productos')
-except:
-    print('fallo dia%')
 
-try:
-    df,lista,dat=get_dia2(producto)
-    print('dia%', len(lista),' productos')
-except:
-    print('fallo dia2')
+class API:
+  def __init__(self):
+    self.url_base='https://7pyngmccwa.execute-api.us-east-1.amazonaws.com/default/apitest?q='
+    self.data=pd.DataFrame(columns=['id', 'query', 'name', 'price', 'url', 'seller','fecha'])
 
-try:
-    df,lista,dat=get_carrefour(producto)
-    print('carrefour', len(lista),' productos')
-except:
-    print('fallo carrefour')
-try:
-    df,lista,dat=get_changomas(producto)
-    print('chango', len(lista),' productos')
-except:
-    print('fallo chango')
+  def get(self,producto):
+    dt = datetime.now()
+    df=pd.DataFrame.from_dict(rq.get(self.url_base+producto).json(),orient='index')
+    time.sleep(0.1)
+    df.loc[:,'query']=producto
+    df.loc[:,'fecha']=dt.strftime('%y-%m-%d')
+    self.data=pd.concat([self.data,df])
+ 
+api=API()
+
+
+api.get('criollitas')
+
+api.get('galletitas melba')
+
+api.get('arroz gallo 1kg')
+
+api.get('harina trigo 1kg blancaflor 000')
+
+
+api.get('harina de maiz')
+
+api.get('fideos tallarin matarazzo')
+
+api.get('papa negra 1kg')
+
+api.get('batata 1kg')
+
+api.get('azucar ledesma')
+
+api.get('mermelada Campagnola')
+
+api.get('lenteja 500')
+
+api.get('cebolla 1kg')
+
+api.get('banana')
+
+api.get('carne picada')
+
+api.get('pate')
+
+api.get('paleta cocida')
+
+api.get('huevos blanco')
+
+api.get('leche entera serenisima')
+
+api.get('queso casancrem')
+
+api.get('yoghurt entero')
+
+api.get('manteca serenisima')
+
+api.get('aceite cocinero girasol')
+
+api.get('coca cola 1,75')
+
+api.get('heineken 473')
+
+api.get('celusal')
+
+api.get('mayona hellmans')
+
+api.get('vinagre menoyo')
+
+api.get('cafe molido cabrales 500')
+
+api.get('yerba mate taragui')
+
+api.data.to_csv('data.csv',index=False)
